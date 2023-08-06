753c11c3bf8c0cb93928087126a35f484b2028a6
Aniruddh Dadhich <aniruddhddh17@gmail.com>
2023-08-06 23:25:31 +0530

    Basic CRUD using Mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -77,18 +77,39 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<updated tour>",
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // by def, mongoose return the doc before update but if new:true then it will return updated doc.
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: "bad request",
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      message: "id not found",
+    });
+  }
 };
